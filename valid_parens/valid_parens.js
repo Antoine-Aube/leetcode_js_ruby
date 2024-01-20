386ab5d1db998b0b2740479d00b72ed9530de501
Antoine Aube <aaube3@gmail.com>
2024-01-20 10:32:28 -0700

    js solution for valid parens

---
+++ valid_parens/valid_parens.js
@@ -0,0 +1,34 @@
+const isValid = (s) => {
+  const pairs = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    //This is really slow because Object.values create a new array
+    //each time...
+    //You can also use new Set for the opening brackets to
+    //directly check that set for those elements,
+    //rather than relying on the hash
+    // Can also just check if it exists with
+    // if (pairs[s[i]])
+    if (Object.values(pairs).includes(s[i])) {
+      stack.push(s[i]);
+      console.log(stack);
+    } else if (pairs[s[i]] === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
+
+let string = "[()]";
+let string_1 = "[()]}";
+
+console.log(isValid(string));
+console.log(isValid(string_1));
