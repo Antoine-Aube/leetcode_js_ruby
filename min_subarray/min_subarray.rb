ba53ac06c72fe96f0d9c0fbeb1b96de7dad6aa3b
Antoine Aube <aaube3@gmail.com>
2024-01-23 15:10:02 -0700

    min subarray sliding window problem rubby solution

---
+++ min_subarray/min_subarray.rb
@@ -0,0 +1,32 @@
+#if sum of the entire array = target, return the length of the nums
+# of if any of the elements in the array = target return one
+# ^ these might and probably should be taken care of within the algo
+#only case that potentially needs to be handled before is if 
+#sum of array < target
+
+#potential solutions
+#a counter, that as you iterate keeps track of the amount of numbers that you've iterated over
+
+def min_sub_array_len(target, nums)
+  start_i = 0
+  sum = 0
+  min_length = Float::INFINITY
+
+  nums.each_with_index do |num, end_i|
+    sum += num
+
+    while sum >= target
+      min_length = [min_length, end_i - start_i + 1].min
+      sum -= nums[start_i]
+      start_i += 1
+    end
+  end
+
+  return min_length == Float::INFINITY ? 0 : min_length
+end
+
+
+nums = [2,3,1,2,4,3,3,1,2,1,1,2,2,4]
+target = 8
+
+puts min_sub_array_len(target, nums)
